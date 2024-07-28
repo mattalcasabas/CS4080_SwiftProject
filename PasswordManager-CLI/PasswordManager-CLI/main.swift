5b9ae1b41afcb27c3c7e13bd36df54786d3c8516
Reveling-h <158114814+Reveling-h@users.noreply.github.com>
2024-07-28 00:05:14 -0700

    Create library directory

--- PasswordManager-CLI/PasswordManager-CLI/main.swift
+++ PasswordManager-CLI/PasswordManager-CLI/main.swift
@@ -4,8 +4,49 @@
 //
 //  Created by Matthew Alcasabas on 7/27/24.
 //
-
 import Foundation
 
-print("Hello, World!")
+func getLibrary() -> Int {
+    print("1. Create new password libary")
+    print("2. Open existing password libary")
+    let choiceNum = Int(readLine() ?? "-1") ?? -1
+    return choiceNum
+}
 
+func createLibrary() -> String {
+    let fileManager = FileManager.default
+    print("Enter file path for the new password library:")
+    let directory = readLine()!
+    
+    do {
+        try fileManager.createDirectory(atPath: directory, withIntermediateDirectories: false)
+        print("Created library at \(directory)")
+        return directory
+    } catch {
+        print("Error, cannot create library")
+        return "e"
+    }
+}
+
+func openPasswordLibrary() {
+    
+}
+
+
+var stayInProgram = true
+var curDirectory: String
+print("Welcome to the password manager!")
+
+repeat {
+    var libraryChoice = getLibrary()
+    switch libraryChoice {
+    case 1:
+        curDirectory = createLibrary()
+    case 2:
+        print(2)
+    default:
+        print("Invalid entry, try again")
+        continue
+    }
+    stayInProgram = false
+} while stayInProgram
