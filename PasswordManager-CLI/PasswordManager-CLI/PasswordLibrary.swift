819be42ba475a50f231e782d1767653d76c83c1c
Matthew Alcasabas <malcasabas28@icloud.com>
2024-08-03 14:17:32 -0700

    working on moving CRUD operations to PasswordLibrary.swift

--- PasswordManager-CLI/PasswordManager-CLI/PasswordLibrary.swift
+++ PasswordManager-CLI/PasswordManager-CLI/PasswordLibrary.swift
@@ -13,9 +13,49 @@ class PasswordLibrary {
     // URL for the file path of the current password library file (optional)
     private var storageURL: URL?
     
+    // initialize PasswordLibrary, or load a library from a given URL
+    init(storageURL: URL? = nil) {
+        self.storageURL = storageURL
+        if let url = storageURL {
+            loadLibrary(from: url)
+        }
+    }
+    
     // save the entries to a JSON file, after updating the array
     private func saveEntries() {
-        // TODO: encode the entries as a JSON, then save it to the URL
+        guard let url = storageURL else {
+            print("No storage URL set.")
+            return
+        }
+        do {
+            let jsonEncoder = JSONEncoder()
+            let data = try jsonEncoder.encode(entries)
+            try data.write(to: url)
+            print("All changes saved.")
+        } catch {
+            // print the error if we couldn't save to library
+            print("Error saving to library: \(error)")
+        }
+    }
+    
+    // load entries from a JSON file
+    private func loadEntries() {
+        guard let url = storageURL else {
+            print("No storage URL set.")
+            return
+        }
+        do {
+            // load data from file
+            let data = try Data(contentsOf: url)
+            // create new JSON decoder
+            let jsonDecoder = JSONDecoder()
+            // decode JSON entries
+            entries = try jsonDecoder.decode([PasswordEntry].self, from: data)
+            print("Successfully loaded \(url).")
+        } catch {
+            // print the error if we couldn't save to library
+            print("Error reading from library: \(error)")
+        }
     }
     
     func addEntry(siteName: String, username: String, password: String) {
@@ -23,6 +63,8 @@ class PasswordLibrary {
         let newEntry = PasswordEntry(siteName: siteName, username: username, password: password)
         // add this entry to the entries array
         entries.append(newEntry)
+        // save new entry to disk
+        saveEntries()
     }
     
     // delete an entry from the array using the given UUID
@@ -31,17 +73,65 @@ class PasswordLibrary {
         entries.removeAll {
             $0.id == id
         }
+        // save changes to disk
+        saveEntries()
     }
     
+    // list all password entries
     func listEntries() {
         // TODO: print out all password entries to the console
+        print("Entries:")
+        // iterate thru each entry and print site name and username
+        for (i, entry) in entries.enumerated() {
+            print("\(i+1): \(entry.siteName), \(entry.username)")
+        }
     }
     
-    func loadLibrary() {
-        // TODO: load a password library file from disk
+    // print out a specific entry for the given index
+    func printEntry(at index: Int) -> PasswordEntry? {
+        // check if index number is valid
+        guard index >= 0 && index < entries.count else {
+            print("printEntry(): Index \(index) is invalid.")
+            return nil
+        }
+        
+        // if valid index, return the given entry
+        return entries[index]
     }
     
-    func saveLibrary() {
-        // TODO: save a password library file to disk
+    // update a specific entry given an index
+    func updateEntry(at index: Int, siteName: String?, username: String?, password: String?) {
+        guard index >= 0 && index < entries.count else {
+            print("updateEntry(): Index \(index) is invalid.")
+            return
+        }
+        
+        // if given site name, update site name
+        if let siteName = siteName {
+            entries[index].siteName = siteName
+        }
+        // if given username, update username
+        if let username = username {
+            entries[index].username = username
+        }
+        // if given password, update password
+        if let password = password {
+            entries[index].password = password
+        }
+        
+        // save changes to disk
+        saveEntries()
+    }
+    
+    // load the password file from a specific URL
+    func loadLibrary(from url: URL) {
+        self.storageURL = url
+        loadEntries()
+    }
+    
+    // save the password file to a specific URL
+    func saveLibrary(to url: URL) {
+        self.storageURL = url
+        saveEntries()
     }
 }
